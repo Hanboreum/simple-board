2dec45fc570954abad53a522140c5a526efd0879
fullm <matoma1_@naver.com>
2024-04-24 23:58:12 +0900

    @PostMapping 추가

--- src/main/java/com/example/simpleboard/post/controller/PostApiController.java
+++ src/main/java/com/example/simpleboard/post/controller/PostApiController.java
@@ -5,6 +5,7 @@ import com.example.simpleboard.post.model.PostRequest;
 import com.example.simpleboard.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostApiController {
     private final PostService postService;
 
+    @PostMapping("")
     public PostEntity create(@Valid PostRequest postRequest){
         return postService.create(postRequest);
     }
