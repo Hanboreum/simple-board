38cd4a32ebac34eba2ed4a83ffa7e9cb14eec3de
fullm <matoma1_@naver.com>
2024-04-24 22:42:56 +0900

    post 관련 db 초기 세팅

---
+++ src/main/java/com/example/simpleboard/post/PostEntity.java
@@ -0,0 +1,32 @@
+package com.example.simpleboard.post;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long boardId;
+    private String userName;
+    private String password;
+    private String email;
+    private String status;
+    private String title;
+    private String content;
+    private Date pastedAt;
+
+}
