5097a23136a5d5217092435aeafe4a3ff17506f9
fullm <matoma1_@naver.com>
2024-04-24 23:07:28 +0900

    @NotBlank

--- src/main/java/com/example/simpleboard/board/model/BoardRequest.java
+++ src/main/java/com/example/simpleboard/board/model/BoardRequest.java
@@ -2,6 +2,7 @@ package com.example.simpleboard.board.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -12,5 +13,7 @@ import lombok.*;
 @AllArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BoardRequest {
+
+    @NotBlank
     private String boardName;
 }
