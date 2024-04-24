54c7ff9e5bf3c41b9150c7dc8de848d912fefd18
fullm <matoma1_@naver.com>
2024-04-24 23:03:09 +0900

    BoardApiController 초기세팅

---
+++ src/main/java/com/example/simpleboard/board/controller/BoardApiController.java
@@ -0,0 +1,15 @@
+package com.example.simpleboard.board.controller;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BoardApiController {
+    private String boardName;
+
+}
---
+++ src/main/java/com/example/simpleboard/board/model/BoardRequest.java
@@ -0,0 +1,12 @@
+package com.example.simpleboard.board.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardRequest {
+}
