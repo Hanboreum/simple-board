89bd8e5f60bf08bfaea1171e215cce6867ff8a37
fullm <matoma1_@naver.com>
2024-04-25 17:14:15 +0900

    postId추가

--- src/main/java/com/example/simpleboard/reply/db/ReplyEntity.java
+++ src/main/java/com/example/simpleboard/reply/db/ReplyEntity.java
@@ -18,6 +18,7 @@ public class ReplyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long postId;
     private String userName;
     private String password;
     private String status;
