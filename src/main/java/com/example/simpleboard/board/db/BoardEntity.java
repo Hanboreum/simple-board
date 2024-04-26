153a8de1b22dabff1f80f0a829feb56bcb2684bc
fullm <matoma1_@naver.com>
2024-04-26 12:52:45 +0900

    주석 추가

--- src/main/java/com/example/simpleboard/board/db/BoardEntity.java
+++ src/main/java/com/example/simpleboard/board/db/BoardEntity.java
@@ -24,6 +24,7 @@ public class BoardEntity {
 
     //1: N 관계
     //entity안에 변수가 있으면 컬럼으로 보기 떄문에
+    //mappedBy 를 했기에 boardConverter에서 연결된 n개의 리스트를 가져오게됨
     @OneToMany (mappedBy = "board") //board 가 postentity 안에 있어야 함
     private List<PostEntity> postList = List.of();
 }
