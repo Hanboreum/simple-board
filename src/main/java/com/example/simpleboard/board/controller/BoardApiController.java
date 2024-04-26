728428377e3ebc3617576323c0c693e893b60f0c
fullm <matoma1_@naver.com>
2024-04-26 12:16:55 +0900

    특정 보드 조회

--- src/main/java/com/example/simpleboard/board/controller/BoardApiController.java
+++ src/main/java/com/example/simpleboard/board/controller/BoardApiController.java
@@ -5,11 +5,9 @@ import com.example.simpleboard.board.model.BoardRequest;
 import com.example.simpleboard.board.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+@Slf4j
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
@@ -19,5 +17,11 @@ public class BoardApiController {
     @PostMapping("")
     public BoardEntity create(@Valid @RequestBody BoardRequest boardRequest){
         return boardService.create(boardRequest);
+    }
+    @GetMapping("id/{id}")
+    public BoardEntity view(@PathVariable Long id){
+        var entity = boardService.view(id);
+        log.info("result : {}", entity);
+        return boardService.view(id);
     }
 }
