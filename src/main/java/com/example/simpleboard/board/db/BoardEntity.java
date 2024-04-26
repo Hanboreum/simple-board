8be2db050b8ebc1bc3e511a39e48d8995b6b65a7
fullm <matoma1_@naver.com>
2024-04-26 15:01:30 +0900

    삭제된 리스트 제외, 아이디 역순 정렬

--- src/main/java/com/example/simpleboard/board/db/BoardEntity.java
+++ src/main/java/com/example/simpleboard/board/db/BoardEntity.java
@@ -3,6 +3,7 @@ package com.example.simpleboard.board.db;
 import com.example.simpleboard.post.db.PostEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OrderBy;
 import org.hibernate.annotations.Where;
 
 import java.util.List;
@@ -27,7 +28,8 @@ public class BoardEntity {
     //entity안에 변수가 있으면 컬럼으로 보기 떄문에
     //mappedBy 를 했기에 boardConverter에서 연결된 n개의 리스트를 가져오게됨
     @OneToMany (mappedBy = "board") //board 가 postentity 안에 있어야 함
-   // @Where(clause = "status ='Registerd' ") //조건절. postentity 클래스의 status = Registerd, 즉 삭제되지 않은 것만 가져온다.
     @Builder.Default
+    @Where(clause = "status ='Registerd' ") //조건절. postentity 클래스의 status = Registerd, 즉 삭제되지 않은 것만 가져온다.
+    @OrderBy(clause = "id desc") //id 역순 정렬
     private List<PostEntity> postList = List.of();
 }
